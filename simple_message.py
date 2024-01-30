c0e00d0ef1cb508bea2d78cc0a411bbb432cc26d
neilcpatel <neilcpatel@gmail.com>
2024-01-29 20:54:49 -0500

    testing variables

---
+++ simple_message.py
@@ -0,0 +1,23 @@
+message = "Test document message"
+print(message)
+
+message_2 = "Testing multiple messages"
+print(message_2)
+
+print(message.title())
+print(message.upper())
+print(message.lower())
+
+full_message = f"{message} {message_2}"
+print(full_message)
+first_name = "neil"
+last_name = "patel"
+full_name = f"{first_name} {last_name}"
+
+print(f"Hello, {full_name.title()}!")
+print(f"\n\tHello, {full_name.title()}!")
+
+favorite_language = " python  "
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
