f42270ffc5f53f04205235559d378af16501d3b2
neilcpatel <neilcpatel@gmail.com>
2024-01-29 21:05:44 -0500

    simple math test

---
+++ simple_math.py
@@ -0,0 +1,57 @@
+math = 2 + 3
+print(math)
+
+math = 3 - 2
+print(math)
+
+math = 2 * 3
+print(math)
+
+math = 3 / 2
+print(math)
+
+math = 3**2
+print(math)
+
+math = 3**3
+print(math)
+
+math = 10**6
+print(math)
+
+math = 2 + 3 * 4
+print(math)
+
+math = (2 + 3) * 4
+print(math)
+
+
+math = 0.1 + 0.1
+print(math)
+
+math = 0.2 + 0.2
+print(math)
+
+
+math = 2 * 0.1
+print(math)
+
+math = 2 * 0.2
+print(math)
+
+math = 4 / 2
+print(math)
+
+math = 1 + 3.0
+print(math)
+
+universe_age = 14_000_000_000
+print(universe_age)
+
+x, y, z = 0, 0, 0
+print(x, y, z)
+
+x, y, z = 1, 5, 8
+print(x, y, z)
+
+# Constants usually defined with capital case. e.g. MAX_CONNECTIONS = 5000, would be a constant
